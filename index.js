731fd8cfb275314349d5663c1a0626986d35ba9e
enome <geert.pasteels@gmail.com>
2014-06-16 21:30:04 +0200

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+/** @jsx React.DOM */
+
+import React from 'react';
+
+var Component = React.createClass({
+
+  /* LOGIC */
+
+  /* LIFECYCLE EVENTS */
+
+  /* RENDER */
+
+  render: function () {
+    return <img src='react-image/image.jpg' />;
+  },
+
+});
+
+
+export default Component;
